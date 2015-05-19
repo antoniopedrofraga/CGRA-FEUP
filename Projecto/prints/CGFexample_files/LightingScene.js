fa88bd1cd891013f0848a39d6f88fe0613ee6321
pedrofraga05 <proteck5.pf@gmail.com>
2015-05-19 17:21:15 +0100

    codigo da aula

--- Projecto/prints/CGFexample_files/LightingScene.js
+++ Projecto/prints/CGFexample_files/LightingScene.js
@@ -305,3 +305,18 @@ LightingScene.prototype.update = function(currTime) {
 LightingScene.prototype.doSomething = function() {
 	console.log("Doing Something...");
 };
+
+LightingScene.prototype.processKeyboard = function(event) {
+	// call CGFinterface default code (omit if you want to override)
+	CGFinterface.prototype.processKeyboard.call(this,event);
+	
+	// Check key codes e.g. here: http://www.asciitable.com/
+	// or use String.fromCharCode(event.keyCode) to compare chars
+	
+	// for better cross-browser support, you may also check suggestions on using event.which in http://www.w3schools.com/jsref/event_key_keycode.asp
+	switch (event.keyCode)
+	{
+		case (65):	// only works for capital 'A', as it is
+			console.log("Key 'A' pressed");
+	};
+};
